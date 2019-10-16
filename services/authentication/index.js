e90b5942075672a276e20f854ebe0eebe7197712
Davi Figueiredo <davi.mello.28@gmail.com>
2019-10-16 15:14:08 -0300

    Better resolver and documentation.

--- services/authentication/index.js
+++ services/authentication/index.js
@@ -2,15 +2,24 @@ const model = require('./model.js')
 const debug = require('debug')('gandalf:services:authenticate')
 const PublicError = require('./../../classes/PublicError')
 
-module.exports = async function authenticate (usernameHash, passwordHash, success, fail) {
+/**
+ * Function to authenticate an username hash and a password hash.
+ * @param  {String} usernameHash Hash of the username, Base64 of a SHA256
+ * @param  {String} passwordHash Hash of the password, Base64 of a SHA256
+ * @param  {Function} success    Optional, Callback function in case of success.
+ * @param  {Function} fail       Optional, Error handles in case of failure
+ * @return {Boolean}             A bool true or false, for the authentication.
+ */
+module.exports = async function authenticate (
+  usernameHash,
+  passwordHash,
+  success = () => {},
+  fail = () => {}
+) {
   debug(`Username: ${usernameHash}, Password: ${passwordHash}, Success: ${success}, Fail: ${fail}`)
 
   try {
     const retrievedPasswordHash = await model.getPasswordHash(usernameHash)
-
-    if (retrievedPasswordHash === false) {
-      throw new PublicError(403, 'User not found.')
-    }
 
     if (retrievedPasswordHash !== passwordHash) {
       throw new PublicError(403, 'Username or password incorrect.')
@@ -18,8 +27,10 @@ module.exports = async function authenticate (usernameHash, passwordHash, succes
 
     if (retrievedPasswordHash === passwordHash) {
       success()
+      return true
     }
   } catch (err) {
     fail(err)
+    return false
   }
 }
