b47a6cf25b37d63566996722638d55d40f6962f2
Davi Figueiredo <davi.mello.28@gmail.com>
2019-10-16 15:14:40 -0300

    Better error handling to model in authentication and documentation,

--- services/authentication/model.js
+++ services/authentication/model.js
@@ -1,15 +1,21 @@
 const sqlite = require('sqlite')
 const dbp = sqlite.open('./services/authentication/authentication.db', { Promise, cached: true })
 const SQL = require('sql-template-strings')
+const PublicError = require('./../../classes/PublicError')
 
 module.exports = {
+  /**
+   * Directly get the usernameHash inside the database.
+   * @param  {String} usernameHash User name hash
+   * @return {String}              [description]
+   */
   async getPasswordHash (usernameHash) {
     const db = await dbp
     const res = await db.get(SQL`SELECT passwordHash FROM users WHERE usernameHash=${usernameHash}`)
     if (res) {
       return res.passwordHash
     } else {
-      return false
+      throw new PublicError(403, 'Username or password incorrect.')
     }
   }
 }
